f93eb3347429bef77f1e9d810f616ec868dd5b00
Vishnu Patil <vps.01.04.2006@gmail.com>
2025-06-10 01:22:58 +0530

    Update mux2to1.v

--- mux2to1.v
+++ mux2to1.v
@@ -1,3 +1,8 @@
+ Module: AND Gate
+ Author: Vishnu Patil
+ Date: June 2025
+ Description: Basic 2-input AND gate.
+
 module mux2to1 (input a, input b, input sel, output y);
     assign y = sel ? b : a;
 endmodule
